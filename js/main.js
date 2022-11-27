b75f32b026d4d0d38c10a7a98343f932e5fb6b5b
Taisei <github>
2022-11-27 18:29:51 +0900

    Implement js

---
+++ js/main.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const triggerChecks = document.querySelectorAll('.check_input');
+const signupButton  = document.getElementById('signup_button');
+
+signupButton.disabled = true;
+signupButton.classList.add('is-inactive');
+
+.addEventlistener("change", function() {
+
+  if() {
+
+  } else {
+
+  }
+})
+
+const getCheckedLength = () => document.querySelectorAll('.check_input:checked').length;
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
+チェックボックス（複数）のチェック状況を見て、送信ボタンを有効・無効化させる 
+
+1.取得してくる要素
+- type属性がcheckboxのinput要素を全て
+- id属性がsignup_buttonのbutton要素
+
+2.初期のボタンを無効にする
+
+3.ボタンに無効のcssを適用
+
+4.if文で条件分岐
+- 
+
+*/
+
